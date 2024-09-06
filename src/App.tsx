45e258b5cb5195df1204804233cd9f1f131bb72b
Daniel Sampaio <danielsampaio127@gmail.com>
2024-09-06 20:20:06 -0300

    feat: Ajustando rota

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
 import Menu from './components/Menu';
-import Content from './pages/Content';
+import Admin from './pages/Admin';
+import Home from './pages/Home';
 
 export default function App() {
   return (
       <div className="bg-blue-950 p-3 m-auto flex flex-col items-center justify-center">
         <Menu />
-        <Content />
+        <Routes>
+            <Route path='/admin' element={<Admin />} />
+            <Route path='/' element={<Home />} />
+        </Routes>
       </div>
   );
 }
