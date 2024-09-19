d027271becd0db26661f732b12445841ce55cc7d
saviolimao <saviolimao@hotmail.com>
2024-09-19 14:50:17 -0300

    Contato-Criado

--- src/pages/Contato.tsx
+++ src/pages/Contato.tsx
@@ -1,12 +1,26 @@
 export default function Contato() {
     return (
         <>
-            <div className="h-screen flex items-center justify-center flex-row gap-5">
-                
-                <span className="pt-24 text-white text-xl"><a href="https://github.com/saviolimao" target="_blank">SavioLimao</a></span>
-                <span className="pt-24 text-white text-xl"><a href="https://github.com/JeffLeite91" target="_blank">JeffMilk</a></span>
-                <span className="pt-24 text-white text-xl"><a href="https://github.com/XDanielSampaioX" target="_blank">XDanielSampaioX</a></span>
+            <div className="h-screen flex items-center justify-center">
+                <div className="flex flex-row gap-5 outline outline-white rounded-lg p-4 items-center h-32">
+                    <span className="text-white text-xl">
+                        <a href="https://github.com/saviolimao" target="_blank" className="hover:text-blue-400" >SavioLimao</a>
+                    </span>
+                    <span className="text-white text-xl">
+                        <a href="https://github.com/JeffLeite91" target="_blank"
+                            className="hover:text-blue-400" >JeffMilk</a>
+                    </span>
+                    <span className="text-white text-xl">
+                        <a href="https://github.com/XDanielSampaioX" target="_blank"
+                            className="hover:text-blue-400" >XDanielSampaioX</a>
+                    </span>
+                    <span className="text-white text-xl">
+                        <a href="https://github.com/kaiomendes15" target="_blank"
+                            className="hover:text-blue-400" >KaioMendes</a>
+                    </span>
+                </div>
             </div>
+
         </>
     )
 };
---
+++ src/pages/PoliticadePrivacidade.tsx
@@ -0,0 +1,15 @@
+export default function PoliticaPri() {
+
+  return (
+    <>
+      <div className="text-white h-screen">
+        <h1 className="pt-36 pb-10 text-center text-xl">
+          Sobre Nós
+        </h1>
+        <p className="px-24 text-center justi">
+          
+        </p>
+      </div>
+    </>
+  )
+}
