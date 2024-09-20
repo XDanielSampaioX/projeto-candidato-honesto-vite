906337fd9b06eac6b7159299fe8a1ef709f34a0c
Kaio <kakacnmendes@gmail.com>
2024-09-20 10:40:01 -0300

    Contato

--- src/pages/Contato.tsx
+++ src/pages/Contato.tsx
@@ -4,15 +4,16 @@ import kaio from '../fotos/kaio.png'
 export default function Contato() {
     return (
         <>
-            <div className="h-screen w-full  flex items-center justify-center encapsulador">
-                <ProfileCard name="seu-nome"  linkedin="SEU-LINK" github="SEU-LINK" instagram="SEU-LINK" />
+            <div className="h-screen w-full alinhamento">
+                <h2>Nossa equipe</h2>
+                <div className="h-screen w-full  flex items-center justify-center encapsulador">
+                    <ProfileCard name="seu-nome"  linkedin="SEU-LINK" github="SEU-LINK" instagram="SEU-LINK" />
                 
-                <ProfileCard name="seu-nome"  linkedin="SEU-LINK" github="SEU-LINK" instagram="SEU-LINK" />
-
-                <ProfileCard name="Kaio Mendes" profilePic={kaio} linkedin="https://www.linkedin.com/in/kaiomendesbr/" github="https://github.com/kaiomendes15" instagram="https://www.instagram.com/kaiomendes__/" />
-
-                <ProfileCard name="seu-nome"  linkedin="SEU-LINK" github="SEU-LINK" instagram="SEU-LINK" />
+                    <ProfileCard name="seu-nome"  linkedin="SEU-LINK" github="SEU-LINK" instagram="SEU-LINK" />
+                    <ProfileCard name="Kaio Mendes" profilePic={kaio} linkedin="https://www.linkedin.com/in/kaiomendesbr/" github="https://github.com/kaiomendes15" instagram="https://www.instagram.com/kaiomendes__/" />
+                    <ProfileCard name="seu-nome"  linkedin="SEU-LINK" github="SEU-LINK" instagram="SEU-LINK" />
                 
+                </div>
             </div>
 
         </>
